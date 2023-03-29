ceab4aff8e591cf0f2e9baf1ca414d56174f40ee
lex0819 <lexmolnar@gmail.com>
2023-03-29 20:37:08 +0300

    regex comment

--- FindDigitsAndLetters.java
+++ FindDigitsAndLetters.java
@@ -4,11 +4,11 @@
 public class FindDigitsAndLetters {
     public static void main(String[] args){
         String data_string = "Absd 123cgk'hgvk       ljuh%vlj fghj sdfg 4gjh 246 Pjdfh! 35637 367834783 145r145r1??? ..";
-        System.out.println(data_string.length());
+//        System.out.println(data_string.length());
 
         data_string = data_string.trim().replaceAll("[ .?!'%]", "");
-        System.out.println(data_string.length());
-
+//        System.out.println(data_string.length());
+ß
         int letters = data_string.replaceAll ("\\d", "").length();
         System.out.printf("There are %d letters\n", letters);
         int digits = data_string.replaceAll ("[a-z,A-Z]", "").length();
