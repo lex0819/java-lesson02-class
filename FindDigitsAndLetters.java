7c34a1301a2907544adf7bce06b0db0a8facdf66
lex0819 <lexmolnar@gmail.com>
2023-03-29 20:49:57 +0300

    string.cahrAt()

--- FindDigitsAndLetters.java
+++ FindDigitsAndLetters.java
@@ -4,11 +4,11 @@
 public class FindDigitsAndLetters {
     public static void main(String[] args){
         String data_string = "Absd 123cgk'hgvk       ljuh%vlj fghj sdfg 4gjh 246 Pjdfh! 35637 367834783 145r145r1??? ..";
-//        System.out.println(data_string.length());
+        //System.out.println(data_string.length());
 
         data_string = data_string.trim().replaceAll("[ .?!'%]", "");
-//        System.out.println(data_string.length());
-ß
+        //System.out.println(data_string.length());
+
         int letters = data_string.replaceAll ("\\d", "").length();
         System.out.printf("There are %d letters\n", letters);
         int digits = data_string.replaceAll ("[a-z,A-Z]", "").length();
@@ -22,5 +22,19 @@ public class FindDigitsAndLetters {
             System.out.print("There are equil digits and letters.\n");
         }
 
+        //string.charAt(item) returns code of item
+        int digit_counts = 0;
+        int letter_counts = 0;
+        data_string = data_string.toLowerCase();
+
+        for (int i = 0; i < data_string.length(); i++) {
+            if(data_string.charAt(i) >= '0' && data_string.charAt(i) <= '9'){
+                digit_counts++;
+            } else if (data_string.charAt(i) >= 'a' && data_string.charAt(i) <= 'z') {
+                letter_counts++;
+            }
+        }
+
+        System.out.printf("There are %d letters and %d digits.\n", letter_counts, digit_counts);
     }
 }
