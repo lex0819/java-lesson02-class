a0848cfdd72fbc66a2027a6b85109420a9b151a0
lex0819 <lexmolnar@gmail.com>
2023-03-29 20:35:57 +0300

    regex

--- FindDigitsAndLetters.java
+++ FindDigitsAndLetters.java
@@ -3,13 +3,13 @@
 
 public class FindDigitsAndLetters {
     public static void main(String[] args){
-        String data_string = "Absd 123cgkhgvkljuhvlj fghj sdfg 4gjh 246 Pjdfh! 35637 367834783 145r145r1??? ..";
-        //System.out.println(data_string.length());
+        String data_string = "Absd 123cgk'hgvk       ljuh%vlj fghj sdfg 4gjh 246 Pjdfh! 35637 367834783 145r145r1??? ..";
+        System.out.println(data_string.length());
 
-        data_string = data_string.trim().replaceAll("\\s+", "").replaceAll("!", "").replaceAll("\\.", "").replaceAll("\\?", "");
-        //System.out.println(data_string.length());
+        data_string = data_string.trim().replaceAll("[ .?!'%]", "");
+        System.out.println(data_string.length());
 
-        int letters = data_string.replaceAll ("[0-9]", "").length();
+        int letters = data_string.replaceAll ("\\d", "").length();
         System.out.printf("There are %d letters\n", letters);
         int digits = data_string.replaceAll ("[a-z,A-Z]", "").length();
         System.out.printf("There are %d digits\n", digits);
